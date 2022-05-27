08e9a65b120b4bb73acc53c10d0fdb0e986124de
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-05-27 09:28:16 +0200

    reports module

---
+++ reports_konery/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Reports Konery & ST ",
+    "summary": "Reports form sale order and invoice.",
+    "version": "14.0.1.0.0",
+    "category": "Reports",
+    "author": "Pedro Guirao, ",
+    "website": "https://github.com/OCA/account-analytic",
+    "license": "AGPL-3",
+    "depends": ['account'],
+    "data": [
+        "views/report.xml",
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
