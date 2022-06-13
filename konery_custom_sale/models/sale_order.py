3b5585630e1f59f84003064346246b3903e30e0c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-06-13 18:59:44 +0200

    rev1 acp

--- konery_custom_sale/models/sale_order.py
+++ konery_custom_sale/models/sale_order.py
@@ -6,12 +6,12 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order',
 
-    all_revision_ids = fields.Many2many('sale.order', string="Revisions", compute="get_all_revisions")
+    all_revision_ids = fields.Many2many('sale.order', string="Revisions", compute="get_all_revisions", domain='['|',('active','=',True),('active','=',False)]')
     all_mail_messages = fields.Many2many('mail.message', string="Messages", compute="get_all_messages", store=False)
 
     def get_all_revisions(self):
         revision = self.env['sale.order'].search([('unrevisioned_name', '=', self.unrevisioned_name),'|',
-                                                  ('active','=',True),('active','=',False)])
+                                                  ('active','in',[True,False])])
         self.all_revision_ids = [(6, 0, revision.ids)]
 
     def get_all_messages(self):
