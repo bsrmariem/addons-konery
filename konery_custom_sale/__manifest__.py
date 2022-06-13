ebcd49a6d9cc8d887269a989ad7816fdc10b6690
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-06-13 16:27:54 +0200

    konery custom sale

---
+++ konery_custom_sale/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Konery Custom Sale",
+    'summary': """
+        
+        """,
+    'author': "Pedro Guirao",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Tools',
+    'version': '14.0.1.0.0',
+    'depends': [
+        'sale_order_revision',
+    ],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ konery_custom_sale/models/sale_order.py
@@ -0,0 +1,20 @@
+# Copyright
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models, api
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order',
+
+    all_revision_ids = fields.Many2many('sale.order', string="Revisions", compute="get_all_revisions")
+    all_mail_messages = fields.Many2many('mail.message', compute="get_all_messages", store=False)
+
+    def get_all_messages(self):
+        messages = self.env['mail.message'].search([('model', '=', 'sale.order'),
+                                                    ('id', 'in', self.all_revision_ids.ids)])
+        all_mail_messages = [(6, 0, messages.ids)]
+
+    def get_all_revisions(self):
+        revision = self.env['sale.order'].search([('unrevisioned_name', '=', self.unrevisioned_name)])
+        all_revision_ids = [(6, 0, revision.ids)]
+
