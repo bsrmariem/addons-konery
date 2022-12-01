7b597cdfe1758136384fa4420ec772e1d9530e90
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-12-02 00:32:05 +0100

    crm tasks v0

--- crm_lead_tasks/models/crm_lead.py
+++ crm_lead_tasks/models/crm_lead.py
@@ -5,3 +5,6 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     task_ids = fields.One2many('project.task', 'lead_id', store=True)
+    def get_tasks_count(self):
+        self.task_count = len(self.tas_ids)
+    task_count = fields.Integer('Tasks', compute=get_tasks_count, store=False)
