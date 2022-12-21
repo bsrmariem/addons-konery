8e5bfaefada94f8ea0974aa3db9d759aef49d369
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-12-21 10:56:39 +0100

    crm_konery

---
+++ crm_konery/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'CRM lead tasks',
+    'version': '16.0.1.0.0',
+    'category': '',
+    'description': u"""
+CRM Konery Customs (kw)
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_lead_views.xml',
+    ],
+    'installable': True,
+}
