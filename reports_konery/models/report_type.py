233ec481f318f8e3aae14ad1f7b754734da3e156
pedroguirao <pedro@serincloud.com>
2023-02-01 13:03:42 +0100

    Reports_ko

--- reports_konery/models/report_type.py
+++ reports_konery/models/report_type.py
@@ -6,11 +6,11 @@ from datetime import datetime
 class ReportType(models.Model):
     _name = 'report.type'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Nombre')
     type_pdf_watermark = fields.Binary(
         "Watermark", help="Upload an pdf file to use as an watermark on this report."
     )
-    report_id = fields.Many2one('ir.actions.report', string='Report')
-    model = fields.Char(string='Model', related='report_id.model')
-    template = fields.Many2one('ir.ui.view', string='Template', required=True)
-    paperformat_id = fields.Many2one('report.paperformat', 'Paper Format')
+    report_id = fields.Many2one('ir.actions.report', string='Informe')
+    model = fields.Char(string='Modelo', related='report_id.model')
+    template = fields.Many2one('ir.ui.view', string='Plantilla', required=True)
+    paperformat_id = fields.Many2one('report.paperformat', 'Formato Papel')
