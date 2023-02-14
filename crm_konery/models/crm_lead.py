d900c5cc29144a9e16e96929325f3b527c46efc5
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-14 11:07:10 +0100

    crm opportunity mandatory and others

--- crm_konery/models/crm_lead.py
+++ crm_konery/models/crm_lead.py
@@ -5,5 +5,5 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     power_kw = fields.Float(string='Power (kw)', store=True)
-    crm_user_id = fields.Many2one('CRM Salesman', related='opportunity_id.user_id')
-    crm_team_id = fields.Many2one('CRM Team', related='opportunity_id.team_id')
+    crm_user_id = fields.Many2one('CRM Salesman', related='opportunity_id.user_id', readonly="False")
+    crm_team_id = fields.Many2one('CRM Team', related='opportunity_id.team_id', readonly="False")
