daf82b1b39e068742a9533885e4d4903f7f218af
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-15 13:14:47 +0100

    crm opportunity mandatory and others

--- crm_konery/models/sale_order.py
+++ crm_konery/models/sale_order.py
@@ -6,7 +6,7 @@ class SaleOrder(models.Model):
 
     crm_user_id = fields.Many2one('res.users', string='CRM Salesman',
                                   related='opportunity_id.user_id', readonly=False,
-                                  domain="['&', ('share', '=', False), ('company_ids', 'in', user_company_ids)]",
+                                  domain="['&', ('share', '=', False), ('company_ids', 'in', crm_user_company_ids)]",
                                   check_company=True, index=True, tracking=True)
     crm_team_id = fields.Many2one('crm.team', string='CRM Team', related='opportunity_id.team_id', readonly=False)
     crm_user_company_ids = fields.Many2many('res.company', related='opportunity_id.user_company_ids', store=False)
