4ef189e3bb686c24b56265a13376e4a1091e9a16
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-15 12:53:10 +0100

    crm opportunity mandatory and others

--- crm_konery/models/sale_order.py
+++ crm_konery/models/sale_order.py
@@ -4,5 +4,5 @@ from odoo import _, api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    crm_user_id = fields.Many2one('CRM Salesman', related='opportunity_id.user_id', readonly="False")
-    crm_team_id = fields.Many2one('CRM Team', related='opportunity_id.team_id', readonly="False")
+    crm_user_id = fields.Many2one('res.users', string='CRM Salesman', related='opportunity_id.user_id', readonly="False")
+    crm_team_id = fields.Many2one('crm.team', string='CRM Team', related='opportunity_id.team_id', readonly="False")
