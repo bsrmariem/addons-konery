6d97031fa104db80a93e5e81a2c5f51539a1db47
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-02 17:28:20 +0100

    konery_rrhh v0 menus

---
+++ konery_rrhh/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Konery Custom RRHH",
+    'summary': """
+        Custom RRHH
+        """,
+    'author': "Serincloud SL",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Tools',
+    'version': '16.0.1.0.0',
+    'depends': [
+        'hr',
+    ],
+    'data': [
+        'views/menu_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
