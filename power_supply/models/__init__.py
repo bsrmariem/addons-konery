57dcba37af935b69c760ef34ba1d148061be286b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-28 00:52:31 +0200

    power supply v0

--- power_supply/models/__init__.py
+++ power_supply/models/__init__.py
@@ -1,6 +1,6 @@
+from . import res_partner
 from . import power_contract
 from . import power_contract_type
 from . import power_pricelist
 from . import power_supply
 from . import power_tag
-from . import res_partner
