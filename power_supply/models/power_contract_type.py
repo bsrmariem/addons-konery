ebb377199e30c80de9a2e1094fb68ddaddae076b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-28 01:12:19 +0200

    power supply v0

--- power_supply/models/power_contract_type.py
+++ power_supply/models/power_contract_type.py
@@ -3,7 +3,7 @@
 
 from odoo import fields, models, api
 
-class PowerTag(models.Model):
+class PowerContractType(models.Model):
     _name = 'power.contract.type'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Power Contract Type'
