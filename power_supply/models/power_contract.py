07c1d436fe1598dad3b3c0482a8a16da9d94b42a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-28 18:27:26 +0200

    power supply v0

--- power_supply/models/power_contract.py
+++ power_supply/models/power_contract.py
@@ -9,6 +9,7 @@ class PowerContract(models.Model):
     _description = 'Power Contracts'
 
     name = fields.Char(string='Name', required=True)
+    active = fields.Boolean('Active')
     date = fields.Date(string='Date contracted')
     date_start = fields.Date(string='Date start')
     date_end = fields.Date(string='Date end')
