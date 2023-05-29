7e2becaeaddc2ac1458934bc8f8b9621fec1b955
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-29 13:55:10 +0200

    project_task_dpt

---
+++ project_task_department/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Project task departments",
+    'summary': """
+        Proyect Task departments
+        """,
+    'author': "Serincloud SL",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Project',
+    'version': '16.0.1.0.0',
+    'depends': [
+        'project',
+        'hr',
+    ],
+    'data': [
+        'views/project_task_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
