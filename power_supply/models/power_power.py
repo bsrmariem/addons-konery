9d4eeec6209d267da3c29f915dc4b5438d6a86e6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-12 19:18:43 +0200

    sup 1206

--- power_supply/models/power_power.py
+++ power_supply/models/power_power.py
@@ -14,13 +14,13 @@ class PowerPower(models.Model):
     partner_id = fields.Many2one('res.partner', related='supply_id.partner_id', string='Customer', store=True)
 
     date_on = fields.Date(string='Activation date')
-    qgas = fields.Float(string='Q', store=True)
-    p1 = fields.Float(string='P1', store=True)
-    p2 = fields.Float(string='P2', store=True)
-    p3 = fields.Float(string='P3', store=True)
-    p4 = fields.Float(string='P4', store=True)
-    p5 = fields.Float(string='P5', store=True)
-    p6 = fields.Float(string='P6', store=True)
+    qgas = fields.Float(string='Q (kWh/day)', store=True)
+    p1 = fields.Float(string='P1 (kW)', store=True)
+    p2 = fields.Float(string='P2 (kW)', store=True)
+    p3 = fields.Float(string='P3 (kW)', store=True)
+    p4 = fields.Float(string='P4 (kW)', store=True)
+    p5 = fields.Float(string='P5 (kW)', store=True)
+    p6 = fields.Float(string='P6 (kW)', store=True)
 
     @api.depends('supply_id', 'date_on')
     def _get_power_power_name(self):
