839a83c11ec358d87ed233590a62fa1261bec423
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-20 18:12:38 +0200

    traducción qgas

--- power_supply/models/power_contract.py
+++ power_supply/models/power_contract.py
@@ -33,7 +33,7 @@ class PowerContract(models.Model):
 
     @api.onchange('date_start','date_end')
     def _check_valid_date(self):
-        if (self.date_start) and (self.supply_id.id):
+        if (self.id) and (self.date_start) and (self.supply_id.id):
             contracts = self.env['power.contract'].search([('id','!=',self.id),('supply_id','=',self.supply_id.id)])
             if contracts.ids:
                 raise ValidationError('hola')
