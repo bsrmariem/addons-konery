0b1008fb1bddbbbf55f158995c18bc4e4feda20c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-29 11:38:07 +0200

    report_konery search customer

--- reports_konery/__manifest__.py
+++ reports_konery/__manifest__.py
@@ -27,7 +27,7 @@
         "views/templates.xml",
         "views/ir_action_report.xml",
         "views/form_views.xml",
-        "views/res_parter_views.xml",
+        "views/res_partner_views.xml",
         #"data/paper_format.xml",
         "security/ir.model.access.csv",
     ],
