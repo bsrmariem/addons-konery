18b977236793adcb20f7d88dcb9304fcba22fb02
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-02 17:19:05 +0200

    bug konery customer

--- reports_konery/models/res_partner.py
+++ reports_konery/models/res_partner.py
@@ -9,8 +9,8 @@ class ResPartner(models.Model):
         for record in self:
             konery_customer = False
             konery_invoices = self.env['account.move'].search([('partner_id','=',record.id),
-                                                               ('move_type','in',['out_invoice'])])
-#                                                               ('report_type.konery','=',True)])
+                                                               ('move_type','in',['out_invoice']),
+                                                               ('report_type.konery','=',True)])
 #            konery_sales = self.env['sale.order'].search([('partner_id','=',record.id),('report_type.konery','=',True)])
 #            if (konery_invoices.ids != False) or (konery_sales.ids != False):
 #                konery_customer = True
