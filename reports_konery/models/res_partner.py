5e1804414708d65dd7cf207b2377b033de7fca86
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-02 13:28:03 +0200

    bug konery customer

--- reports_konery/models/res_partner.py
+++ reports_konery/models/res_partner.py
@@ -7,8 +7,8 @@ class ResPartner(models.Model):
     @api.depends('invoice_ids','invoice_ids.report_type.konery','sale_order_ids','sale_order_ids.report_type.konery')
     def get_konery_customer(self):
         konery_customer = False
-        konery_invoices = self.env['account.move'].search([('move_type','=','out_invoice')])
-        konery_sales = self.env['sale.order'].search([('report_type.konery','=',True)])
+        konery_invoices = self.env['account.move'].search([('partner_id','=',self.id),('move_type','in',['out_invoice')]])
+        konery_sales = self.env['sale.order'].search([('partner_id','=',self.id),('report_type.konery','=',True)])
 #        if (konery_invoices != False) or (konery_sales != False):
 #            konery_customer = True
         self.konery_customer = konery_customer
@@ -17,9 +17,11 @@ class ResPartner(models.Model):
     @api.depends('invoice_ids','invoice_ids.report_type.solarteam','sale_order_ids','sale_order_ids.report_type.solarteam')
     def get_solarteam_customer(self):
         solarteam_customer = False
-        solarteam_invoices = self.env['account.move'].search([('move_type','in',['out_invoice']),
+        solarteam_invoices = self.env['account.move'].search([('partner_id','=',self.id),
+                                                              ('move_type','in',['out_invoice']),
                                                               ('report_type.solarteam','=',True)])
-        solarteam_sales = self.env['sale.order'].search([('report_type.solarteam','=',True)])
+        solarteam_sales = self.env['sale.order'].search([('partner_id','=',self.id),
+                                                         ('report_type.solarteam','=',True)])
 #        if (solarteam_invoices.ids) or (solarteam_sales.ids):
 #            solarteam_customer = True
 #        self.solarteam_customer = solarteam_customer
