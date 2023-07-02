5961cdfb2f749f029e79f3044f634371a5f7765c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-02 13:34:49 +0200

    bug konery customer

--- reports_konery/models/res_partner.py
+++ reports_konery/models/res_partner.py
@@ -8,22 +8,25 @@ class ResPartner(models.Model):
     def get_konery_customer(self):
         for record in self:
             konery_customer = False
-            konery_invoices = self.env['account.move'].search([('partner_id','=',record.id),('move_type','in',['out_invoice'])])
+            konery_invoices = self.env['account.move'].search([('partner_id','=',record.id),
+                                                               ('move_type','in',['out_invoice']),
+                                                               ('report_type.konery','=',True)])
             konery_sales = self.env['sale.order'].search([('partner_id','=',record.id),('report_type.konery','=',True)])
-    #        if (konery_invoices != False) or (konery_sales != False):
-    #            konery_customer = True
+            if (konery_invoices != False) or (konery_sales != False):
+                konery_customer = True
             record['konery_customer'] = konery_customer
     konery_customer = fields.Boolean('Konery customer', store=True, compute=get_konery_customer)
 
     @api.depends('invoice_ids','invoice_ids.report_type.solarteam','sale_order_ids','sale_order_ids.report_type.solarteam')
     def get_solarteam_customer(self):
-        solarteam_customer = False
-        solarteam_invoices = self.env['account.move'].search([('partner_id','=',self.id),
-                                                              ('move_type','in',['out_invoice']),
-                                                              ('report_type.solarteam','=',True)])
-        solarteam_sales = self.env['sale.order'].search([('partner_id','=',self.id),
-                                                         ('report_type.solarteam','=',True)])
-#        if (solarteam_invoices.ids) or (solarteam_sales.ids):
-#            solarteam_customer = True
-#        self.solarteam_customer = solarteam_customer
+        for record in self:
+            solarteam_customer = False
+            solarteam_invoices = self.env['account.move'].search([('partner_id','=',record.id),
+                                                                  ('move_type','in',['out_invoice']),
+                                                                  ('report_type.solarteam','=',True)])
+            solarteam_sales = self.env['sale.order'].search([('partner_id','=',record.id),
+                                                             ('report_type.solarteam','=',True)])
+            if (solarteam_invoices.ids) or (solarteam_sales.ids):
+                solarteam_customer = True
+            record['solarteam_customer'] = solarteam_customer
     solarteam_customer = fields.Boolean('Konery customer', store=True, compute=get_solarteam_customer)
