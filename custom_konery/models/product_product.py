48bf6bb2d9f64cd0ca18a3ff4c51c3c008519184
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-16 13:45:49 +0200

    bug letra

--- custom_konery/models/product_product.py
+++
@@ -1,7 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    standard_price = fields.Float(tracking=100)
--- custom_konery/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    standard_price = fields.Float(tracking=100)
