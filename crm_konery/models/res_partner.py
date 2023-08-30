b82537a17ce93cd71cef32e341d9af062bc5b895
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-30 18:37:47 +0200

    upd crm-leads

--- crm_konery/models/res_partner.py
+++ crm_konery/models/res_partner.py
@@ -4,8 +4,6 @@ from odoo import _, api, fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    # Pendiente de incluir en init de modelos cuando completen todos los vat que faltan:
-    vat = fields.Char(required=True)
     power_kw = fields.Float(string='Power (kw)', store=True)
     fv_origin = fields.Selection([('fv_konery','FV Konery'),('fv_other','FV others'),('none','None')],
                                  string='FV Origin', store=True)
