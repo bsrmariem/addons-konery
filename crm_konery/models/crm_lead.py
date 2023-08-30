f6337182c4b498434e06e4492321587e6f82a38b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-30 10:42:42 +0200

    upd crm-leads

--- crm_konery/models/crm_lead.py
+++ crm_konery/models/crm_lead.py
@@ -5,3 +5,5 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     power_kw = fields.Float(string='Power (kw)', store=True)
+    fv_origin = fields.Selection([('fv_konery','FV Konery'),('fv_other','FV others'),('none','None')],
+                                 string='FV Origin', store=True)
