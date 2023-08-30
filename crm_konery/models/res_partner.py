a4871e774dc1cb425ab4c853a593454064b0739b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-30 18:28:42 +0200

    upd crm-leads

--- crm_konery/models/res_partner.py
+++ crm_konery/models/res_partner.py
@@ -6,3 +6,6 @@ class ResPartner(models.Model):
 
     # Pendiente de incluir en init de modelos cuando completen todos los vat que faltan:
     vat = fields.Char(required=True)
+    power_kw = fields.Float(string='Power (kw)', store=True)
+    fv_origin = fields.Selection([('fv_konery','FV Konery'),('fv_other','FV others'),('none','None')],
+                                 string='FV Origin', store=True)
